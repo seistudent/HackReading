917e15b7d361f6a906a17744eeb4b4ca7fb3ca82
daryl <darylpoon@gmail.com>
2020-02-15 14:53:50 +0800

    user authentication done

--- hackreading_app_api/server.js
+++ hackreading_app_api/server.js
@@ -22,30 +22,48 @@ mongoose.connect(mongoURI, { useNewUrlParser: true },
 
 // Middleware
 app.use(express.json()); //use .json(), not .urlencoded()
-// app.use(cors());
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (process.env.allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(express.static("public"));
+app.use(
+    session({
+        secret: "mutusamy chen",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+app.use(cors());
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // allow requests with no origin 
+//         // (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (process.env.allowedOrigins.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 // Error / Disconnection
 db.on('error', err => console.log(err.message + ' is Mongod not running?'))
 db.on('disconnected', () => console.log('mongo disconnected'))
 
+// Routes
+const notesController = require('./controller/notes.js');
+app.use('/notes', notesController);
+const sessionsController = require("./controller/sessions.js");
+app.use("/sessions", sessionsController);
+const usersController = require("./controller/users.js");
+app.use("/users", usersController);
+
+// this will catch any route that doesn't exist
+app.get("*", (req, res) => {
+    res.status(404).json("Sorry, page not found");
+});
+
 app.listen(PORT, () => {
     console.log('Let\'s get things done on port', PORT)
 })
 
-// Routes
-const notesController = require('./controllers/notes.js');
-app.use('/notes', notesController);
 
